61bcf15dee123f9e6df6c3c642cf1dd98f1e9af4
Eugene Nesterov <en170901@gmail.com>
2024-12-11 18:19:55 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthorService(ApplicationDbContext context) 
+        { 
+            _context = context;
+        }
+
+        /// <summary>
+        /// Get authors who wrote an even number of books published after 2015
+        /// </summary>
+        public Task<List<Author>> GetAuthors()
+        {
+            return _context.Authors.AsNoTracking()
+                .Where(author => author.Books.Count(book => book.PublishDate.Year > 2015) % 2 == 0 &&
+                                 author.Books.Any(book => book.PublishDate.Year > 2015))
+                .ToListAsync();
+        }
+
+        /// <summary>
+        /// Get the author who wrote the book with the longest title
+        /// </summary>
+        public Task<Author> GetAuthor()
+        {
+            return _context.Books.AsNoTracking()
+                .OrderByDescending(book => book.Title.Length)
+                .ThenBy(book => book.AuthorId)
+                .Select(book => book.Author)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class BookService : IBookService
+    {
+        private readonly ApplicationDbContext _context;
+        public BookService(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Get Books that contain "Red" in the title and were published after the release of Sabaton's "Carolus Rex" album
+        /// </summary>
+        public Task<List<Book>> GetBooks()
+        {
+            var carolusRexReleaseDate = new DateTime(2012, 5, 22);
+
+            return _context.Books.AsNoTracking()
+                .Where(book => book.Title.Contains("Red")
+                               && book.PublishDate > carolusRexReleaseDate)
+                .ToListAsync();
+        }
+
+        /// <summary>
+        /// Get the Book with the highest cost of the published edition
+        /// </summary>
+        public Task<Book> GetBook()
+        {
+            return _context.Books.AsNoTracking()
+                .OrderByDescending(book => book.Price * book.QuantityPublished)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
